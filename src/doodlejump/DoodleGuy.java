c8361d84e9f1d2e8aebd06869be3fe6e06f71a22
ffuis-aljosa <Korisnik@FF-206-MAIN-W7>
2016-03-21 11:20:21 +0100

    Listening to keyboard input Teleporting on sides of screen

--- src/doodlejump/DoodleGuy.java
+++ src/doodlejump/DoodleGuy.java
@@ -13,6 +13,9 @@ public class DoodleGuy {
     private int velocityY;
     
     private final int GRAVITY = 1;
+    private final int HORIZONTAL_SPEED = 10;
+    
+    private final int TELEPORT_BUFFER = 40;
 
     public DoodleGuy(int panelWidth, int panelHeight) {
         this.x = (panelWidth - WIDTH) / 2;
@@ -23,8 +26,36 @@ public class DoodleGuy {
         velocityY = 0;
     }
     
+    public void moveLeft() {
+        velocityX = -HORIZONTAL_SPEED;
+    }
+    
+    public void moveRight() {
+        velocityX = HORIZONTAL_SPEED;
+    }
+    
+    public void stopHorizontal() {
+        velocityX = 0;
+    }
+    
     public boolean isAtBottom(int panelHeight) {
         return y + HEIGHT >= panelHeight;
+    }
+    
+    public boolean isMaxLeft() {
+        return x <= -TELEPORT_BUFFER;
+    }
+    
+    public boolean isMaxRight(int panelWidth) {
+        return x + WIDTH >= panelWidth + TELEPORT_BUFFER;
+    }
+    
+    public void teleportRight(int panelWidth) {
+        x = panelWidth - WIDTH;
+    }
+    
+    public void teleportLeft() {
+        x = 0;
     }
     
     public void draw(Graphics2D g2D) {
--- src/doodlejump/DoodlePanel.java
+++ src/doodlejump/DoodlePanel.java
@@ -6,54 +6,88 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-public class DoodlePanel extends JPanel implements ActionListener {
+public class DoodlePanel extends JPanel implements ActionListener, KeyListener {
 
     private final Timer timer;
     private final int FRAMES_PER_SECOND = 30;
-    
+
     private DoodleGuy dGuy;
-    
+
     public DoodlePanel() {
         setBackground(Color.WHITE);
-        
+
         timer = new Timer(1000 / FRAMES_PER_SECOND, this);
         timer.start();
+        
+        setFocusable(true);
+        addKeyListener(this);
     }
-    
+
     public void startGame() {
         Dimension size = this.getSize();
-        
+
         dGuy = new DoodleGuy(size.width, size.height);
     }
 
     @Override
     public void paint(Graphics g) {
         super.paint(g);
-        
+
         Graphics2D g2D = (Graphics2D) g;
-        
+
         dGuy.draw(g2D);
     }
 
     private void moveObjects() {
         dGuy.move();
     }
-    
+
     private void handleCollisions() {
         Dimension size = this.getSize();
-        
-        if (dGuy.isAtBottom(size.height))
+
+        if (dGuy.isAtBottom(size.height)) {
             dGuy.bounce();
+        }
+        
+        if (dGuy.isMaxLeft())
+            dGuy.teleportRight(size.width);
+        else if (dGuy.isMaxRight(size.width))
+            dGuy.teleportLeft();
     }
-    
+
     @Override
     public void actionPerformed(ActionEvent e) {
         moveObjects();
         handleCollisions();
         repaint();
     }
-    
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int keyCode = e.getKeyCode();
+
+        if (keyCode == KeyEvent.VK_LEFT) {
+            dGuy.moveLeft();
+        } else if (keyCode == KeyEvent.VK_RIGHT) {
+            dGuy.moveRight();
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int keyCode = e.getKeyCode();
+        
+        if (keyCode == KeyEvent.VK_LEFT || keyCode == KeyEvent.VK_RIGHT) {
+            dGuy.stopHorizontal();
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {}
+
 }
